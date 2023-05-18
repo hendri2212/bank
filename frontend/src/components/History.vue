14026d194fd728413456f5defa86c10df32daa64
hendri2212 <cowok_cool320@yahoo.co.id>
2023-05-18 18:39:24 +0800

    add axios

--- frontend/src/components/History.vue
+++ frontend/src/components/History.vue
@@ -25,7 +25,7 @@
                     <td class="text-end">2000</td>
                 </tr>
                 <tr>
-                    <td>Menabung</td>
+                    <td>Menabung {{ this.data }}</td>
                     <td class="text-center">K</td>
                     <td class="text-end">1000</td>
                     <td class="text-end">1000</td>
@@ -34,3 +34,19 @@
         </table>
     </main>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                data: '100'
+            }
+        },
+        created(){
+            axios
+            .get('https://smkn1kotabaru.sch.id/api/data_siswa')
+            .then(function (response){
+                console.log(response.data)
+            })
+        }
+    }
+</script>
