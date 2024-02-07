e84f425da0cde4e5f9c2ce928f4443daff259dff
hendri2212 <cowok_cool320@yahoo.co.id>
2024-02-08 01:12:08 +0800

    fix error saldo undefined

--- frontend/src/components/Balance.vue
+++ frontend/src/components/Balance.vue
@@ -1,22 +1,21 @@
-<script setup>
-    import Navbar from '../components/Navbar.vue'
-</script>
 <template>
     <Navbar />
     <main class="container bg-white py-2">
         <span class="fw-bold">Total Your Balance</span>
         <br>
-        <!-- <span class="h3 text-success">Rp 0,00</span> -->
-        <span class="h3 text-success" v-if="this.saldo==undefined">Rp 0,00</span>
-        <span class="h3 text-success" v-else>{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(this.saldo) }}</span>
+        <span class="h3 text-success">{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(this.saldo) }}</span>
         <!-- <span class="h3 text-success">{{ new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(this.balance) }}</span> -->
     </main>
 </template>
 <script>
 // import axios from "axios"
+import Navbar from '../components/Navbar.vue'
 import { useCounterStore } from '../stores/counter.js'
 
 export default {
+    components: {
+        Navbar
+    },
     data(){
         return {
             saldo : null
@@ -30,8 +29,11 @@ export default {
             }
         })
         .then(response => {
-            this.saldo = response.data.balance
-            // console.log(response.data.balance)
+            if(response.data.balance == null || response.data.balance == undefined){
+                this.saldo = 0
+            }else{
+                this.saldo = response.data.balance
+            }
         })
     }
 }
